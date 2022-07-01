8cb3f784169897e9ea1c84f408d07a99496a3b4c
HUES4X <HUES4X@HUES4X.local>
2022-07-01 23:44:39 +0800

    Responsive Deployment

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,19 @@
+import { ImageBoxContainer, ImageContainer } from "./Header.styles";
+import Card from "./Card/Card";
+
+const Header = () => {
+  return (
+    // <HeaderContainer>
+    <ImageBoxContainer>
+      <ImageContainer
+        src="images/header/header_gradient1.svg"
+        alt="banner Image"
+        height={450}
+      />
+      <Card />
+    </ImageBoxContainer>
+    // </HeaderContainer>
+  );
+};
+
+export default Header;
